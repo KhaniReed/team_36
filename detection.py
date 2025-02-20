3376d22db31a09e0ef08bdd7b4309a305ad1d3f5
KhaniReed <khanireed1@gmail.com>
2025-02-20 15:40:47 -0500

    Removed these two files due to combining both URL and email content detection in one file.

--- detection.py
+++
@@ -1,8 +0,0 @@
-#suspicious link detector
-import re
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import joblib
